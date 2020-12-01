9994578c157b8c195e7513bdf6b61e803470446b
むずでょ <muzudho1@gmail.com>
2020-12-01 19:55:34 +0900

    ログ・ディレクトリが無いというエラーだったのかだぜ☆（＾～＾）

--- frontend/app/webapp.py
+++ frontend/app/webapp.py
@@ -1,3 +1,23 @@
-from module.app import get_app
+# from module.app import get_app
 
-app = get_app()
+# app = get_app()
+
+from flask import Flask
+app = Flask(__name__)
+
+def get_app():
+    return app
+
+@app.route('/')
+def hello():
+    name = "Hello World"
+    return name
+
+@app.route('/good')
+def good():
+    name = "Good"
+    return name
+
+## おまじない
+if __name__ == "__main__":
+    app.run(debug=True)
